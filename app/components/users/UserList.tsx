05fcee22744a0d401297701559e9acd8f4a8179a
AbbasAli <abbasali.alphasquad@gmail.com>
2024-09-25 17:03:37 +0500

    update

--- app/components/users/UserList.tsx
+++ app/components/users/UserList.tsx
@@ -14,8 +14,8 @@ import { toast } from "react-toastify";
 import ViewUserDialog from "./ViewUserDialog";
 import EditUserDialog from "./EditUserDialog";
 import AddNewUserDialog from "./AddNewUserDialog";
+import Loader from "../utils/loader";
 import axios from "axios";
-import db from "../../../db/db.json";
 import { api } from "../api";
 const UserList = () => {
   const [userData, setUserData] = useState<any>([]);
@@ -64,69 +64,77 @@ const UserList = () => {
   };
   return (
     <div className="w-full ">
-      <Table className="w-full overflow-hidden">
-        <TableCaption>A list of Users </TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="">ID</TableHead>
-            <TableHead className="">Name</TableHead>
-            <TableHead>User Name</TableHead>
-            <TableHead>Email</TableHead>
-            <TableHead className="text-right">Operation</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {userData &&
-            userData.length > 0 &&
-            userData.map((userItem: any, index: any) => {
-              return (
-                <TableRow key={index}>
-                  <TableCell className="font-medium">{userItem?.id}</TableCell>
-                  <TableCell>{userItem.name}</TableCell>
-                  <TableCell>{userItem.username}</TableCell>
-                  <TableCell>{userItem.email}</TableCell>
-                  <TableCell className="flex gap-3 justify-end">
-                    <Button
-                      onClick={() => {
-                        setSelectedUserData(userItem);
-                        setOpenViewDialog(true);
-                      }}
-                    >
-                      View
-                    </Button>
-                    <Button
-                      variant="secondary"
-                      onClick={() => {
-                        setSelectedUserData(userItem);
-                        setOpenEditDialog(true);
-                      }}
-                    >
-                      Edit
-                    </Button>
-                    <Button
-                      variant="destructive"
-                      onClick={() => {
-                        deleteUser(userItem);
-                      }}
-                    >
-                      Delete
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-        </TableBody>
-      </Table>
-      <div className="w-full py-5 text-right px-5">
-        <Button
-          onClick={() => {
-            setSelectedUserData(null);
-            setOpenNewUserDialog(true);
-          }}
-        >
-          Add New User
-        </Button>
+{userData&&userData.length>0?
+     <>
+   
+     <Table className="w-full overflow-hidden">
+          <TableCaption>A list of Users </TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="">ID</TableHead>
+              <TableHead className="">Name</TableHead>
+              <TableHead>User Name</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead className="text-right">Operation</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {userData &&
+              userData.length > 0 &&
+              userData.map((userItem: any, index: any) => {
+                return (
+                  <TableRow key={index}>
+                    <TableCell className="font-medium">{userItem?.id}</TableCell>
+                    <TableCell>{userItem.name}</TableCell>
+                    <TableCell>{userItem.username}</TableCell>
+                    <TableCell>{userItem.email}</TableCell>
+                    <TableCell className="flex gap-3 justify-end">
+                      <Button
+                        onClick={() => {
+                          setSelectedUserData(userItem);
+                          setOpenViewDialog(true);
+                        }}
+                      >
+                        View
+                      </Button>
+                      <Button
+                        variant="secondary"
+                        onClick={() => {
+                          setSelectedUserData(userItem);
+                          setOpenEditDialog(true);
+                        }}
+                      >
+                        Edit
+                      </Button>
+                      <Button
+                        variant="destructive"
+                        onClick={() => {
+                          deleteUser(userItem);
+                        }}
+                      >
+                        Delete
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+        <div className="w-full py-5 text-right px-5">
+          <Button
+            onClick={() => {
+              setSelectedUserData(null);
+              setOpenNewUserDialog(true);
+            }}
+          >
+            Add New User
+          </Button>
+        </div>
+     
+     </>:<div className="w-screen h-[60vh] flex justify-center items-center">
+     <Loader/>
       </div>
+}
       {openViewDialog && (
         <ViewUserDialog
           selectedUserData={selectedUserData}
